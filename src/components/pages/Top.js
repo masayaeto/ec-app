68aaed450369f3a93d6fcc11dce4490f19fc90fa
衞藤勝也 <masaya@etomasauyanoMacBook-Pro.local>
2021-03-06 00:35:43 +0900

    stateの作成

--- src/components/pages/Top.js
+++ src/components/pages/Top.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import ProductGrid from '../ProductGrid/ProductGrid'
 import Layout from '../Layout/Layout'
 import ProductGridItem from '../ProductGridItem/ProductGridItem'
 
 const Top = () => {
+  useEffect(() => {
+    
+  }, [])
   return (
     <div>
       <Layout/>
