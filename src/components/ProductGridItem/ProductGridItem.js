840ab9518734474b7f3b7a37dafa502f438ce017
衞藤勝也 <masaya@etomasauyanoMacBook-Pro.local>
2021-03-09 00:42:48 +0900

    YahooAPIへの接続と売れ筋ランキングの取得

--- src/components/ProductGridItem/ProductGridItem.js
+++ src/components/ProductGridItem/ProductGridItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ProductGridItem = ({child}) => {
   return (
     <div>
-      <h1>商品一覧</h1>
+      
     </div>
   )
 }
--- src/components/pages/Top.js
+++ src/components/pages/Top.js
@@ -1,19 +1,33 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useContext} from 'react'
 import ProductGrid from '../ProductGrid/ProductGrid'
 import Layout from '../Layout/Layout'
 import ProductGridItem from '../ProductGridItem/ProductGridItem'
+import { fetchPopularData } from '../../apis/index'
+import { Store } from '../../store/index'
 
 const Top = () => {
+
+  const {globalState, setGlobalState} = useContext(Store)
+
   useEffect(() => {
-    
-  }, [])
+    fetchPopularData().then((response) => {
+      setGlobalState({type: "SET_RANKING", ranking: response})
+    }, []).catch(() => {
+      console.log('通信に失敗しました。');
+    })
+  })
   return (
     <div>
       <Layout/>
       <ProductGrid>
-        <ProductGridItem>
-          
-        </ProductGridItem>
+        {console.log(globalState)}
+        { globalState.ranking && globalState.ranking.map((rank) => {
+          return (
+            <ProductGridItem id="">
+              
+            </ProductGridItem>
+          )
+        })}
       </ProductGrid>
     </div>
   )
